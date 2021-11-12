3004efd3bc6b821376b8b22ec7b1a27d6f4d1d34
Tausif4171 <tausifkhan4171@gmail.com>
2021-11-13 03:11:22 +0530

    Updated file

--- pages/forms/cricket/index.php
+++ pages/forms/cricket/index.php
@@ -36,6 +36,56 @@
             margin-top: 20px;
         }
 
+
+        table {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        border-collapse: collapse;
+        width: 800px;
+        height: 200px;
+        border: 1px solid #bdc3c7;
+        box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);
+    }
+    
+    tr {
+        transition: all .2s ease-in;
+        cursor: pointer;
+    }
+    
+    th,
+    td {
+        padding: 12px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+    
+    #header {
+        background-color: #16a085;
+        color: #fff;
+    }
+    
+    h1 {
+        font-weight: 600;
+        text-align: center;
+        background-color: #16a085;
+        color: #fff;
+        padding: 10px 0px;
+    }
+    
+    tr:hover {
+        background-color: #f5f5f5;
+        transform: scale(1.02);
+        box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);
+    }
+    
+    @media only screen and (max-width: 768px) {
+        table {
+            width: 90%;
+        }
+    }
+
     </style>
     <!-- Custom styles for this template -->
     <link href="https://getbootstrap.com/docs/5.0/examples/starter-template/starter-template.css" rel="stylesheet">
@@ -145,10 +195,142 @@ while($res = mysqli_fetch_array($query)){
         
     </main>
     <!-- /.container -->
+
+    <h1 style=" margin-bottom:500px">Tournament Score Table</h1>
+    <!-- <hr> -->
+
+    <table style="margin-top: 940px; margin-bottom:700px">
+        <tr id="header">
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Age</th>
+            <th>Country</th>
+            <th>Gender</th>
+        </tr>
+        <tr>
+            <td>John </td>
+            <td>Doe </td>
+            <td>25 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>steve </td>
+            <td>Doe </td>
+            <td>28 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>simo </td>
+            <td>Doe </td>
+            <td>26 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>karim </td>
+            <td>Doe </td>
+            <td>21 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>adam </td>
+            <td>Doe </td>
+            <td>20 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>keven </td>
+            <td>Doe </td>
+            <td>20 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+
+    </table>
+
+    <h1 style=" margin-bottom:700px">Venue</h1>
+    <!-- <hr> -->
+
+    <table style="margin-top: 1510px ">
+        <tr id="header">
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Age</th>
+            <th>Country</th>
+            <th>Gender</th>
+        </tr>
+        <tr>
+            <td>John </td>
+            <td>Doe </td>
+            <td>25 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>steve </td>
+            <td>Doe </td>
+            <td>28 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>simo </td>
+            <td>Doe </td>
+            <td>26 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>karim </td>
+            <td>Doe </td>
+            <td>21 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>adam </td>
+            <td>Doe </td>
+            <td>20 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+        <tr>
+            <td>keven </td>
+            <td>Doe </td>
+            <td>20 </td>
+            <td>USA </td>
+            <td>Male </td>
+
+        </tr>
+
+    </table>
+
+
+
+
+
+
     <div class="footer" style="background-color: #333; height:50px;">
     <br>
         <h6 style="text-align: center;margin-top:-10px; color: white;">Copyright © 2021 <a href="index.php">Cricket League Tournament</a>. All rights reserved.</h6>
 </div>
+
+
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
